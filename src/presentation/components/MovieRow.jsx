2d0e32181750bed77682a4f9f2aed800725c9628
Steliana Dimitrova <steliana.dimitrova18@gmail.com>
2026-03-18 23:54:37 +0200

    Refactor components

--- src/presentation/components/MovieRow.jsx
+++ src/presentation/components/MovieRow.jsx
@@ -1,7 +1,7 @@
 import MovieCard from "./MovieCard";
 import classes from "./MovieRow.module.css";
 
-export default function MovieRow({ rowName, movies }) {
+export default function MovieRow({ rowName, movies, type = 'movie' }) {
   return (
     <section className={classes.movieRowSection}>
       <h2 className={classes.rowName}>{rowName}</h2>
@@ -9,7 +9,7 @@ export default function MovieRow({ rowName, movies }) {
         <div className={classes.moviesContainer}>
           {movies.map(
             (movie) =>
-              movie.backdrop_path && <MovieCard key={movie.id} movie={movie} />,
+              movie.backdrop_path && <MovieCard key={movie.id} movie={movie} type={type}/>,
           )}
         </div>
       </div>
