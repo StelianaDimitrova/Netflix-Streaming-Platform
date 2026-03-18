cbd07239f4e28a27d90a0257a6071f0c2b355ec9
Steliana Dimitrova <steliana.dimitrova18@gmail.com>
2026-03-18 23:47:47 +0200

    Refactor route

--- src/presentation/layouts/MainLayout.jsx
+++ src/presentation/layouts/MainLayout.jsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/shows" element={<TVShows />} />
-          <Route path="/watch/:type/:id" element={<MediaPage />} />
+          <Route path="/:type/watch/:type/:id" element={<MediaPage />} />
           <Route path="/mylist" element={<MyList />} />
         </Routes>
       </main>
